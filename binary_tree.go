ff84de3535dc872283d9807455abc12b323b84a9
Rober Morales-Chaparro <rober.morales@gmail.com>
2022-01-01 23:49:22 +0100

    binary_tree

---
+++ binary_tree.go
@@ -0,0 +1,141 @@
+package collections
+
+import (
+	"fmt"
+	"strings"
+)
+
+type binaryTree[T ordered] struct {
+	*binaryTreeNode[T]
+}
+
+type binaryTreeNode[T ordered] struct {
+	value T
+	left  *binaryTreeNode[T]
+	right *binaryTreeNode[T]
+}
+
+type binaryTreeNodeP[T ordered] *binaryTreeNode[T]
+
+func NewBinaryTree[T ordered]() BinaryTree[T] {
+	return &binaryTree[T]{}
+}
+
+type BinaryTree[T ordered] interface {
+	Collection[T]
+
+	Insert(item T)
+	Delete(item T)
+}
+
+func (node *binaryTreeNode[T]) Len() int {
+	if node == nil {
+		return 0
+	}
+	return 1 + node.left.Len() + node.right.Len()
+}
+
+func (node *binaryTreeNode[T]) string() string {
+	sb := &strings.Builder{}
+	if node != nil {
+		if node.left != nil {
+			sb.WriteString(node.left.string())
+			sb.WriteRune(' ')
+		}
+		fmt.Fprint(sb, node.value)
+		if node.right != nil {
+			sb.WriteRune(' ')
+			sb.WriteString(node.right.string())
+		}
+	}
+	return sb.String()
+}
+
+func (node *binaryTreeNode[T]) String() string {
+	sb := &strings.Builder{}
+	sb.WriteRune('[')
+	sb.WriteString(node.string())
+	sb.WriteRune(']')
+	return sb.String()
+}
+
+func (node *binaryTreeNode[T]) each(i *int, do func(index int, item T)) {
+	if node == nil {
+		return
+	}
+
+	node.left.each(i, do)
+
+	do(*i, node.value)
+	*i++
+
+	node.right.each(i, do)
+}
+
+func (node *binaryTreeNode[T]) Each(do func(index int, item T)) {
+	i := 0
+	node.each(&i, do)
+}
+
+func (node *binaryTreeNode[T]) eachUntil(i *int, do func(index int, item T), stop func(index int, item T) bool) {
+	if node == nil {
+		return
+	}
+
+	node.left.eachUntil(i, do, stop)
+
+	do(*i, node.value)
+
+	if stop(*i, node.value) {
+		return
+	}
+
+	*i++
+
+	node.right.eachUntil(i, do, stop)
+}
+
+func (node *binaryTreeNode[T]) EachUntil(do func(index int, item T), stop func(index int, item T) bool) {
+	i := 0
+	node.eachUntil(&i, do, stop)
+}
+
+func (node *binaryTreeNode[T]) insert(item T) *binaryTreeNode[T] {
+	if node == nil {
+		node = &binaryTreeNode[T]{value: item}
+	} else if item < node.value {
+		node.left = node.left.insert(item)
+	} else {
+		node.right = node.right.insert(item)
+	}
+	return node
+}
+
+func (node *binaryTree[T]) Insert(item T) {
+	node.binaryTreeNode = node.binaryTreeNode.insert(item)
+}
+
+func (node *binaryTreeNode[T]) merge(right *binaryTreeNode[T]) *binaryTreeNode[T] {
+	if node == nil {
+		return right
+	} else if right == nil {
+		return node
+	} else {
+		return node.right.merge(right)
+	}
+}
+
+func (node *binaryTreeNode[T]) delete(item T) *binaryTreeNode[T] {
+	if item == node.value {
+		return node.left.merge(node.right)
+	} else if item < node.value {
+		node.left = node.left.insert(item)
+	} else {
+		node.right = node.right.insert(item)
+	}
+	return node
+}
+
+func (node *binaryTree[T]) Delete(item T) {
+	node.binaryTreeNode = node.binaryTreeNode.delete(item)
+}
---
+++ binary_tree_example_test.go
@@ -0,0 +1,84 @@
+package collections_test
+
+import (
+	"fmt"
+
+	"github.com/go-generics/collections"
+)
+
+func ExampleBinaryTree_Insert() {
+	tree := collections.NewBinaryTree[int]()
+
+	tree.Insert(1)
+	tree.Insert(2)
+	tree.Insert(3)
+
+	fmt.Print(tree)
+
+	// Output:
+	// [1 2 3]
+}
+
+func ExampleBinaryTree_Delete() {
+	tree := collections.NewBinaryTree[int]()
+
+	tree.Insert(1)
+	tree.Insert(2)
+	fmt.Println(tree)
+
+	tree.Delete(1)
+	tree.Delete(2)
+	fmt.Println(tree)
+
+	// Output:
+	// [1 2]
+	// []
+}
+
+func ExampleBinaryTree_Len() {
+	tree := collections.NewBinaryTree[int]()
+	for i := 0; i < 1000; i++ {
+		tree.Insert(i)
+	}
+
+	fmt.Println(tree.Len())
+
+	// Output:
+	// 1000
+}
+
+func ExampleBinaryTree_Each() {
+	tree := collections.NewBinaryTree[int]()
+	for i := 0; i < 1000; i++ {
+		tree.Insert(i)
+	}
+
+	sum := 0
+	tree.Each(func(_, item int) {
+		sum += item
+	})
+
+	fmt.Println(sum)
+
+	// Output:
+	// 499500
+}
+
+func ExampleBinaryTree_EachUntil() {
+	tree := collections.NewBinaryTree[int]()
+	for i := 0; i < 1000; i++ {
+		tree.Insert(i)
+	}
+
+	sum := 0
+	tree.EachUntil(func(_, item int) {
+		sum += item
+	}, func(_ int, item int) bool {
+		return item == 3
+	})
+
+	fmt.Println(sum)
+
+	// Output:
+	// 6
+}
