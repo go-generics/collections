804569d50ffd3c021189bdbc408f63f8d6a2431c
Rober Morales-Chaparro <rober.morales@gmail.com>
2022-01-01 23:49:31 +0100

    types

---
+++ types.go
@@ -0,0 +1,8 @@
+package collections
+
+type ordered interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+		~float32 | ~float64 |
+		~string
+}
